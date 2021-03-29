5c994a84f45ad1e48a2a1f09c0a62696d8ac390c
Gerard Gomez <gerardgomez11@me.com>
2021-03-29 19:46:45 -0400

    Initial Commit

---
+++ Shared/Programming_MasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_MasteryApp.swift
+//  Shared
+//
+//  Created by Gerard Gomez on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Programming_MasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
