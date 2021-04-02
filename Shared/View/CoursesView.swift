b05a7f0f16f8b4b768b8467d3f9284cf850a76e6
Gerard Gomez <gerardgomez11@me.com>
2021-04-02 08:23:43 -0400

    Add Course Model Add Courses View Add Courses Item

--- Shared/View/CoursesView.swift
+++ Shared/View/CoursesView.swift
@@ -8,8 +8,64 @@
 import SwiftUI
 
 struct CoursesView: View {
+    
+    @State private var isDisabled = false
+    @State var selectedCourse: Course? = nil
+    @State private var show = false
+    @Namespace var namespace
+    let gridItem: GridItem = GridItem(.adaptive(minimum: 160), spacing: 16)
+    
     var body: some View {
-        Text("")
+        ZStack {
+            ScrollView {
+                LazyVGrid(columns: [gridItem], spacing: 16) {
+                    ForEach(Course.courses) { course in
+                        CourseItem(course: course)
+                            .matchedGeometryEffect(id: course.id, in: namespace, isSource: !show)
+                            .frame(height: 200)
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    show.toggle()
+                                    selectedCourse = course
+                                    isDisabled = true
+                                }
+                            }
+                            .disabled(isDisabled)
+                    }
+                }
+                .padding(16)
+                .frame(maxWidth: .infinity)
+            }
+            
+            if selectedCourse != nil {
+                ScrollView {
+                    CourseItem(course: selectedCourse!)
+                        .matchedGeometryEffect(id: selectedCourse!.id, in: namespace)
+                        .frame(height: 300)
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                show.toggle()
+                                selectedCourse = nil
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    isDisabled = false
+                                }
+                            }
+                        }
+                    VStack(spacing: 10) {
+                        ForEach(0..<20) { item in
+                            CourseRow()
+                        }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                    .padding()
+                }
+                .background(Color("Background 1"))
+                .transition(.asymmetric(
+                                insertion: AnyTransition.opacity.animation(Animation.spring().delay(0.3)),
+                                removal: AnyTransition.opacity.animation(.spring())))
+                .edgesIgnoringSafeArea(.all)
+            }
+        }
     }
 }
 
